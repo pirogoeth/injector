5886890fc9fd862a455e2133742ceb830f1a36ca
Sean Johnson <sean@mailgun.com>
2017-05-21 02:33:13 -0500

    Add parent passing

--- src/injector/__init__.py
+++ src/injector/__init__.py
@@ -3,12 +3,12 @@
 from injector import payload
 
 
-def injector_from_payload(payload_name, as_name='payload'):
+def injector_from_payload(payload_name, **kw):
 
-    return generate_injector(payload.get_payload(payload_name), as_name=as_name)
+    return generate_injector(payload.get_payload(payload_name), **kw)
 
 
-def generate_injector(inj_payload, as_name='payload'):
+def generate_injector(inj_payload, as_name='payload', allow_parent_passing=False):
 
     def injector(func):
 
@@ -16,6 +16,13 @@ def generate_injector(inj_payload, as_name='payload'):
 
             func_g = func.__globals__
             sentinel = object()
+
+            parent_name = 'parent_{}'.format(as_name)
+            parent_prev_g = func_g.get(parent_name, sentinel)
+
+            if allow_parent_passing and parent_name in kw:
+                parent_g = kw.pop(parent_name, None)
+                func_g[parent_name] = parent_g
 
             prev_g = func_g.get(as_name, sentinel)
             func_g[as_name] = inj_payload
@@ -27,6 +34,12 @@ def generate_injector(inj_payload, as_name='payload'):
                     del func_g[as_name]
                 else:
                     func_g[as_name] = prev_g
+
+                if allow_parent_passing and parent_name in func_g:
+                    if parent_prev_g is sentinel:
+                        func_g[parent_name] = None
+                    else:
+                        func_g[parent_name] = parent_prev_g
 
             return res
 
